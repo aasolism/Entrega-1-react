9da310eac9cad2fa7e758e2f4692641a02b51d64
Andres solis <aasolis@uc.cl>
2024-10-08 10:23:44 -0300

    Configure base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Entrega-1-react/', // Ruta base para GitHub Pages
   plugins: [react()],
-})
+});
